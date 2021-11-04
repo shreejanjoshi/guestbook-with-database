8d960982ddd91b1b83de0ecf62709f7b1311912a
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-11-04 18:13:52 +0100

    made a layout for guestbook

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # guestbook-with-database
+
+### What's the Goal
+Here I'am trying to make guestbook with database. Here user can type their name, title, date and text. After they submit their detail can be seen.
+
+This exercise is for myself, to build my knowledge in php and database.
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guest Book</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-xs-6">
+            <form action="index.php" method="post">
+                <h1>Guest Book</h1>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="date">Date</label>
+                    <input type="date" name="date" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea name="message" class="form-control message"style="width:100%;height:150px;"></textarea>
+                </div>
+                <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
